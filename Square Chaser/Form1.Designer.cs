e13ed544bee78de0faac923325462caee441a08b
Saahil Patel <saahpate456@ed.amdsb.ca>
2022-11-29 00:11:27 -0500

    Add project files.

---
+++ Square Chaser/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Square_Chaser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startButton = new System.Windows.Forms.Button();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.player1scoreOutput = new System.Windows.Forms.Label();
+            this.player2scoreOutput = new System.Windows.Forms.Label();
+            this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.Location = new System.Drawing.Point(285, 279);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(80, 40);
+            this.startButton.TabIndex = 1;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.titleLabel.Location = new System.Drawing.Point(237, 224);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(174, 29);
+            this.titleLabel.TabIndex = 2;
+            this.titleLabel.Text = "Square Chaser";
+            // 
+            // player1scoreOutput
+            // 
+            this.player1scoreOutput.AutoSize = true;
+            this.player1scoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player1scoreOutput.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.player1scoreOutput.Location = new System.Drawing.Point(239, 9);
+            this.player1scoreOutput.Name = "player1scoreOutput";
+            this.player1scoreOutput.Size = new System.Drawing.Size(20, 24);
+            this.player1scoreOutput.TabIndex = 3;
+            this.player1scoreOutput.Text = "0";
+            this.player1scoreOutput.Visible = false;
+            this.player1scoreOutput.DoubleClick += new System.EventHandler(this.player1scoreLabel_DoubleClick);
+            // 
+            // player2scoreOutput
+            // 
+            this.player2scoreOutput.AutoSize = true;
+            this.player2scoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.player2scoreOutput.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.player2scoreOutput.Location = new System.Drawing.Point(391, 9);
+            this.player2scoreOutput.Name = "player2scoreOutput";
+            this.player2scoreOutput.Size = new System.Drawing.Size(20, 24);
+            this.player2scoreOutput.TabIndex = 4;
+            this.player2scoreOutput.Text = "0";
+            this.player2scoreOutput.Visible = false;
+            this.player2scoreOutput.DoubleClick += new System.EventHandler(this.player2scoreLabel_DoubleClick);
+            // 
+            // gameTimer
+            // 
+            this.gameTimer.Enabled = true;
+            this.gameTimer.Interval = 20;
+            this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.Controls.Add(this.player2scoreOutput);
+            this.Controls.Add(this.player1scoreOutput);
+            this.Controls.Add(this.titleLabel);
+            this.Controls.Add(this.startButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.Label player1scoreOutput;
+        private System.Windows.Forms.Label player2scoreOutput;
+        private System.Windows.Forms.Timer gameTimer;
+    }
+}
+
---
+++ Square Chaser/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Square_Chaser
+{
+    public partial class Form1 : Form
+    {
+        Rectangle player1 = new Rectangle(150, 150, 35, 35);
+        Rectangle player2 = new Rectangle(450, 150, 35, 35);
+
+        int player1Score = 0;
+        int player2Score = 0;
+
+        int playerSpeed = 7;
+
+        ////Ball speed is for extra edition
+        //int ballXSpeed = -2;
+        //int ballYSpeed = 2;
+
+        bool wDown = false;
+        bool sDown = false;
+        bool upArrowDown = false;
+        bool downArrowDown = false;
+
+        //bool aDown = false;
+        //bool dDown = false;
+        //bool leftDown = false;
+        //bool rightDown = false;
+
+        SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
+        SolidBrush whiteBrush = new SolidBrush(Color.White);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = false;
+                    break;
+                case Keys.S:
+                    sDown = false;
+                    break;
+                case Keys.Up:
+                    upArrowDown = false;
+                    break;
+                case Keys.Down:
+                    downArrowDown = false;
+                    break;
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.W:
+                    wDown = true;
+                    break;
+                case Keys.S:
+                    sDown = true;
+                    break;
+                case Keys.Up:
+                    upArrowDown = true;
+                    break;
+                case Keys.Down:
+                    downArrowDown = true;
+                    break;
+            }
+        }
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.Visible = false;
+            titleLabel.Visible = false;
+            player1scoreOutput.Visible = true;
+            player2scoreOutput.Visible = true;
+
+
+            player1scoreOutput.Text = $"{player1Score}";
+            player2scoreOutput.Text = $"{player2Score}";
+        }
+
+        private void gameTimer_Tick(object sender, EventArgs e)
+        {
+            //move player 1 
+            if (wDown == true && player1.Y > 0)
+            {
+                player1.Y -= playerSpeed;
+            }
+
+            if (sDown == true && player1.Y < this.Height - player1.Height)
+            {
+                player1.Y += playerSpeed;
+            }
+
+            //move player 2 
+            if (upArrowDown == true && player2.Y > 0)
+            {
+                player2.Y -= playerSpeed;
+            }
+
+            if (downArrowDown == true && player2.Y < this.Height - player2.Height)
+            {
+                player2.Y += playerSpeed;
+            }
+
+            Refresh();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(blueBrush, player1);
+            e.Graphics.FillRectangle(blueBrush, player2);
+        }
+
+        private void player1scoreLabel_DoubleClick(object sender, EventArgs e)
+        {
+            player1Score++;
+        }
+
+        private void player2scoreLabel_DoubleClick(object sender, EventArgs e)
+        {
+            player2Score++;
+        }
+    }
+}
