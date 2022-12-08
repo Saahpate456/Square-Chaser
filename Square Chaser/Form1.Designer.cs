dffc5e5c457688885410c5ae062e4776740581ec
Saahil Patel <saahpate456@ed.amdsb.ca>
2022-12-07 20:26:34 -0500

    5.0 changes

--- Square Chaser/Form1.Designer.cs
+++ Square Chaser/Form1.Designer.cs
@@ -29,59 +29,34 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.startButton = new System.Windows.Forms.Button();
-            this.titleLabel = new System.Windows.Forms.Label();
             this.player1scoreOutput = new System.Windows.Forms.Label();
             this.player2scoreOutput = new System.Windows.Forms.Label();
             this.gameTimer = new System.Windows.Forms.Timer(this.components);
+            this.winLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // startButton
-            // 
-            this.startButton.Location = new System.Drawing.Point(285, 279);
-            this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(80, 40);
-            this.startButton.TabIndex = 1;
-            this.startButton.Text = "Start";
-            this.startButton.UseVisualStyleBackColor = true;
-            this.startButton.Click += new System.EventHandler(this.startButton_Click);
-            // 
-            // titleLabel
-            // 
-            this.titleLabel.AutoSize = true;
-            this.titleLabel.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.titleLabel.ForeColor = System.Drawing.SystemColors.ButtonFace;
-            this.titleLabel.Location = new System.Drawing.Point(237, 224);
-            this.titleLabel.Name = "titleLabel";
-            this.titleLabel.Size = new System.Drawing.Size(174, 29);
-            this.titleLabel.TabIndex = 2;
-            this.titleLabel.Text = "Square Chaser";
             // 
             // player1scoreOutput
             // 
             this.player1scoreOutput.AutoSize = true;
             this.player1scoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.player1scoreOutput.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.player1scoreOutput.Location = new System.Drawing.Point(239, 9);
+            this.player1scoreOutput.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.player1scoreOutput.Location = new System.Drawing.Point(237, 9);
             this.player1scoreOutput.Name = "player1scoreOutput";
             this.player1scoreOutput.Size = new System.Drawing.Size(20, 24);
             this.player1scoreOutput.TabIndex = 3;
             this.player1scoreOutput.Text = "0";
-            this.player1scoreOutput.Visible = false;
             this.player1scoreOutput.DoubleClick += new System.EventHandler(this.player1scoreLabel_DoubleClick);
             // 
             // player2scoreOutput
             // 
             this.player2scoreOutput.AutoSize = true;
             this.player2scoreOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.player2scoreOutput.ForeColor = System.Drawing.SystemColors.ControlLight;
-            this.player2scoreOutput.Location = new System.Drawing.Point(391, 9);
+            this.player2scoreOutput.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.player2scoreOutput.Location = new System.Drawing.Point(332, 9);
             this.player2scoreOutput.Name = "player2scoreOutput";
             this.player2scoreOutput.Size = new System.Drawing.Size(20, 24);
             this.player2scoreOutput.TabIndex = 4;
             this.player2scoreOutput.Text = "0";
-            this.player2scoreOutput.Visible = false;
             this.player2scoreOutput.DoubleClick += new System.EventHandler(this.player2scoreLabel_DoubleClick);
             // 
             // gameTimer
@@ -90,16 +65,25 @@
             this.gameTimer.Interval = 20;
             this.gameTimer.Tick += new System.EventHandler(this.gameTimer_Tick);
             // 
+            // winLabel
+            // 
+            this.winLabel.AutoSize = true;
+            this.winLabel.Location = new System.Drawing.Point(238, 156);
+            this.winLabel.Name = "winLabel";
+            this.winLabel.Size = new System.Drawing.Size(0, 13);
+            this.winLabel.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.ClientSize = new System.Drawing.Size(634, 611);
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(600, 450);
+            this.Controls.Add(this.winLabel);
             this.Controls.Add(this.player2scoreOutput);
             this.Controls.Add(this.player1scoreOutput);
-            this.Controls.Add(this.titleLabel);
-            this.Controls.Add(this.startButton);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -112,12 +96,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button startButton;
-        private System.Windows.Forms.Label titleLabel;
         private System.Windows.Forms.Label player1scoreOutput;
         private System.Windows.Forms.Label player2scoreOutput;
         private System.Windows.Forms.Timer gameTimer;
+        private System.Windows.Forms.Label winLabel;
     }
 }
 
--- Square Chaser/Form1.cs
+++ Square Chaser/Form1.cs
@@ -7,18 +7,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
 
 namespace Square_Chaser
 {
     public partial class Form1 : Form
     {
-        Rectangle player1 = new Rectangle(150, 150, 35, 35);
-        Rectangle player2 = new Rectangle(450, 150, 35, 35);
+        Rectangle player1 = new Rectangle(150, 150, 20, 20);
+        Rectangle player2 = new Rectangle(450, 150, 20, 20);
+
+        //Powerups
+        Rectangle speedOrb = new Rectangle();
+        Rectangle slowOrb = new Rectangle();
+        Rectangle point = new Rectangle();
+
+        //Random
+        Random spaceRandom = new Random();
+        //int xRandom = 0;
+        //int yRandom = 0;
+
+
 
         int player1Score = 0;
         int player2Score = 0;
 
-        int playerSpeed = 7;
+        int player1Speed = 7;
+        int player2Speed = 7;
 
         ////Ball speed is for extra edition
         //int ballXSpeed = -2;
@@ -29,13 +43,16 @@ namespace Square_Chaser
         bool upArrowDown = false;
         bool downArrowDown = false;
 
-        //bool aDown = false;
-        //bool dDown = false;
-        //bool leftDown = false;
-        //bool rightDown = false;
+        bool aDown = false;
+        bool dDown = false;
+        bool leftDown = false;
+        bool rightDown = false;
 
         SolidBrush blueBrush = new SolidBrush(Color.DodgerBlue);
         SolidBrush whiteBrush = new SolidBrush(Color.White);
+        SolidBrush orangeBrush = new SolidBrush(Color.Orange);
+        SolidBrush yellowBrush = new SolidBrush(Color.Yellow);
+        SolidBrush redBrush = new SolidBrush(Color.Red);
         public Form1()
         {
             InitializeComponent();
@@ -62,6 +79,18 @@ namespace Square_Chaser
                 case Keys.Down:
                     downArrowDown = false;
                     break;
+                case Keys.D:
+                    dDown = false;
+                    break;
+                case Keys.Right:
+                    rightDown = false;
+                    break;
+                case Keys.Left:
+                    leftDown = false;
+                    break;
+                case Keys.A:
+                    aDown = false;
+                    break;
             }
         }
 
@@ -81,42 +110,141 @@ namespace Square_Chaser
                 case Keys.Down:
                     downArrowDown = true;
                     break;
+                case Keys.D:
+                    dDown = true;
+                    break;
+                case Keys.Right:
+                    rightDown = true;
+                    break;
+                case Keys.Left:
+                    leftDown = true;
+                    break;
+                case Keys.A:
+                    aDown = true;
+                    break;
             }
         }
-        private void startButton_Click(object sender, EventArgs e)
-        {
-            startButton.Visible = false;
-            titleLabel.Visible = false;
-            player1scoreOutput.Visible = true;
-            player2scoreOutput.Visible = true;
-
-
-            player1scoreOutput.Text = $"{player1Score}";
-            player2scoreOutput.Text = $"{player2Score}";
-        }
-
         private void gameTimer_Tick(object sender, EventArgs e)
         {
+            
+
+            //g.Clear(Color.White);
+            //g.DrawLine(borderTop, 40, 40, 100, 40);
+
+            Random spaceRandom = new Random();
+
+            //int xRandom = spaceRandom.Next(1, 600);
+            //int yRandom = spaceRandom.Next(1, 450);
+            //Rectangle slowOrb = new Rectangle(xRandom, yRandom, 7, 7);
+
+            
+
             //move player 1 
             if (wDown == true && player1.Y > 0)
             {
-                player1.Y -= playerSpeed;
+                player1.Y -= player1Speed;
             }
-
             if (sDown == true && player1.Y < this.Height - player1.Height)
             {
-                player1.Y += playerSpeed;
+                player1.Y += player1Speed;
+            }
+            if (dDown == true && player1.X > 0)
+            {
+                player1.X += player1Speed;
+            }
+            if (aDown == true && player1.X > 0)
+            {
+                player1.X -= player1Speed;
             }
 
             //move player 2 
             if (upArrowDown == true && player2.Y > 0)
             {
-                player2.Y -= playerSpeed;
+                player2.Y -= player2Speed;
             }
-
             if (downArrowDown == true && player2.Y < this.Height - player2.Height)
             {
-                player2.Y += playerSpeed;
+                player2.Y += player2Speed;
+            }
+            if (rightDown == true && player2.X > 0)
+            {
+                player2.X += player2Speed;
+            }
+            if (leftDown == true && player2.X > 0)
+            {
+                player2.X -= player2Speed;
+            }
+
+            //check if  players hit  walls
+            //NOTE:Create a method for these
+            if (player1.X <= 0 && player1.X < 600 && player1.Y >= 0)
+            {
+                player1.X = 150;
+                player1.Y = 150;
+            }
+            if (player1.Y <= 600 && player1.Y > 0 && player1.X >= 600)
+            {
+                player1.X = 150;
+                player1.Y = 150;
+            }
+
+            if (player2.X <= 0 && player2.X < 600 && player2.Y >= 0)
+            {
+                player2.X = 450;
+                player2.Y = 150;
+            }
+            if (player2.Y <= 600 && player2.Y > 0 && player2.X >= 600)
+            {
+                player2.X = 450;
+                player2.Y = 150;
+            }
+
+
+
+            //Point powerup
+            if (player1.IntersectsWith(point))
+            {
+                
+                player1Score = player1Score + 1;
+                player1scoreOutput.Text = $"{player1Score}";
+                int xRandom = spaceRandom.Next(1, 600);
+                int yRandom = spaceRandom.Next(1, 450);
+                Rectangle point = new Rectangle(xRandom, yRandom, 7, 7);
+
+            }
+
+            //speed powerup
+            if (player1.IntersectsWith(speedOrb))
+            {
+                player1Speed = player1Speed + 1;
+                int xRandom = spaceRandom.Next(1, 600);
+                int yRandom = spaceRandom.Next(1, 450);
+                Rectangle speedOrb = new Rectangle(xRandom, yRandom, 7, 7);
+            }
+
+            //Slow powerup
+            if (player1.IntersectsWith(slowOrb))
+            {
+                player1Speed = player1Speed - 1;
+                int xRandom = spaceRandom.Next(1, 600);
+                int yRandom = spaceRandom.Next(1, 450);
+                Rectangle slowOrb = new Rectangle(xRandom, yRandom, 7, 7);
+            }
+
+
+
+            //win system
+            if (player1Score == 7)
+            {
+                winLabel.Text = $"Player 1 wins!!! with a score of {player1Score} to {player2Score}";
+                Thread.Sleep(5000);
+                Application.Exit();
+            }
+            if (player2Score == 7)
+            {
+                winLabel.Text = $"Player 2 wins!!! with a score of {player2Score} to {player1Score}";
+                Thread.Sleep(5000);
+                Application.Exit();
             }
 
             Refresh();
@@ -126,6 +254,10 @@ namespace Square_Chaser
         {
             e.Graphics.FillRectangle(blueBrush, player1);
             e.Graphics.FillRectangle(blueBrush, player2);
+            e.Graphics.FillRectangle(orangeBrush, point);
+            e.Graphics.FillRectangle(redBrush, slowOrb);
+            e.Graphics.FillRectangle(yellowBrush, speedOrb);
+
         }
 
         private void player1scoreLabel_DoubleClick(object sender, EventArgs e)
